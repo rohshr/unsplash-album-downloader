245055f536b66a83fd10b643ce50926556ae90c8
Rohan Shrestha <sth.ronx@gmail.com>
2020-01-24 20:35:05 +0545

    Download photos with uploader's name

--- download_user_album.py
+++ download_user_album.py
@@ -12,7 +12,7 @@ APP_ID = '' #please provide your app_id here to download photos
 
 if not APP_ID:
     with open('app_id.txt','rt') as f:
-    	APP_ID = f.readline().strip()
+        APP_ID = f.readline().strip()
 
 def user_parse_args():
     parser = argparse.ArgumentParser(description = 'arguments of how to download photos')
@@ -46,26 +46,26 @@ def get_user_uploads(username, mode):
     save_photos(user_directory,photo_ids)
 
 def get_user_likes(username, mode):
-	user_profile = get_user(username)
-	if user_profile['total_likes'] == 0:
-		print('user have not liked anything. No photos to download')
-		return
-	photo_ids = get_photo_ids(user_profile['links']['likes'], user_profile['total_likes'],mode)
-	user_directory = os.getcwd()+r'/'+user_profile['name']+'-unsplash-likes-'+mode
-	save_photos(user_directory,photo_ids)
+    user_profile = get_user(username)
+    if user_profile['total_likes'] == 0:
+        print('user have not liked anything. No photos to download')
+        return
+    photo_ids = get_photo_ids(user_profile['links']['likes'], user_profile['total_likes'],mode)
+    user_directory = os.getcwd()+r'/'+user_profile['name']+'-unsplash-likes-'+mode
+    save_photos(user_directory,photo_ids)
 
 
 def get_user_collections(username, mode):
-	user_profile = get_user(username)
-	if user_profile['total_collections'] == 0:
-		print('no collections to download')
-		return
-	collection_ids = get_collection_ids(USER_URL+user_profile['username']+'/collections/',user_profile['total_collections'])
-	photo_ids = dict()
-	for cid in collection_ids:
-		photo_ids = get_photo_ids(cid['url'], cid['total_photos'],mode)
-		user_directory = os.getcwd()+r'/'+user_profile['name']+'-unsplash-collections-'+mode+r'/'+cid['title']
-		save_photos(user_directory,photo_ids)
+    user_profile = get_user(username)
+    if user_profile['total_collections'] == 0:
+        print('no collections to download')
+        return
+    collection_ids = get_collection_ids(USER_URL+user_profile['username']+'/collections/',user_profile['total_collections'])
+    photo_ids = dict()
+    for cid in collection_ids:
+        photo_ids = get_photo_ids(cid['url'], cid['total_photos'],mode)
+        user_directory = os.getcwd()+r'/'+user_profile['name']+'-unsplash-collections-'+mode+r'/'+cid['title']
+        save_photos(user_directory,photo_ids)
 
 
 def get_photo_ids(url, total, mode):
@@ -75,39 +75,40 @@ def get_photo_ids(url, total, mode):
         payload = {'client_id':APP_ID,'page':str(page_number),'per_page':'30'}
         photos_list_response, status_code = get_response(url,payload)
         for i in photos_list_response:
-            photo_ids[i['id']] = i['urls'][mode]
+            photo_ids[i['id']] = [i['urls'][mode], i['user']['first_name']+'-'+i['user']['last_name']]
     return photo_ids
 
 
 def get_collection_ids(url, total):
-	total_pages = math.ceil(total/30)
-	collection_ids = list()
-	for page_number in range(1, total_pages+1):
-		payload = {'client_id':APP_ID,'page':str(page_number),'per_page':'30'}
-		collection_ids_list, status_code = get_response(url,payload)
-		for i in collection_ids_list:
-			collection_ids.append({'id':i['id'], 'title':i['title'], 'total_photos':i['total_photos'],'url':i['links']['photos']})
-	return collection_ids
+    total_pages = math.ceil(total/30)
+    collection_ids = list()
+    for page_number in range(1, total_pages+1):
+        payload = {'client_id':APP_ID,'page':str(page_number),'per_page':'30'}
+        collection_ids_list, status_code = get_response(url,payload)
+        for i in collection_ids_list:
+            collection_ids.append({'id':i['id'], 'title':i['title'], 'total_photos':i['total_photos'],'url':i['links']['photos']})
+    return collection_ids
 
 
 def save_photos(user_directory,photo_ids):
-	if not os.path.exists(user_directory):
-		os.makedirs(user_directory)
-	else:
-		photo_ids_local = {f[:-4] for f in os.listdir(user_directory) if f.endswith('.jpg')}
-		for pid in photo_ids_local:
-			try:
-				del photo_ids[pid]
-			except KeyError:
-				print('no photo exists with the ID "{}" on unsplash website'.format(pid))
-	if not photo_ids:
-		sys.exit('all photos already exists in the {} folder'.format(user_directory[user_directory.rfind('/')+1:]))
-	else:
-		for k,v in photo_ids.items():
-			photo_download_response = requests.get(photo_ids[k],stream = True)
-			with open(user_directory+r'/'+k+'.jpg', 'wb') as out_file:
-			    shutil.copyfileobj(photo_download_response.raw, out_file)
-		print('successfully downloaded {} photos in "{}" folder'.format(len(photo_ids), user_directory[user_directory.rfind('/')+1:]))
+    if not os.path.exists(user_directory):
+        os.makedirs(user_directory)
+    else:
+        photo_ids_local = {f[:-4] for f in os.listdir(user_directory) if f.endswith('.jpg')}
+        for pid in photo_ids_local:
+            try:
+                del photo_ids[pid]
+            except KeyError:
+                print('no photo exists with the ID "{}" on unsplash website'.format(pid))
+    if not photo_ids:
+        sys.exit('all photos already exists in the {} folder'.format(user_directory[user_directory.rfind('/')+1:]))
+    else:
+        for k,v in photo_ids.items():
+            print('Downloading photo: {}'.format(v[1]+'-'+k+'.jpg...'))
+            photo_download_response = requests.get(photo_ids[k][0],stream = True)
+            with open(user_directory+r'/'+v[1]+'-'+k+'.jpg', 'wb') as out_file:
+                shutil.copyfileobj(photo_download_response.raw, out_file)
+        print('successfully downloaded {} photos in "{}" folder'.format(len(photo_ids), user_directory[user_directory.rfind('/')+1:]))
 
 
 def user_main():
@@ -118,9 +119,9 @@ def user_main():
     if albumtype == 'uploads':
         get_user_uploads(username, size)
     if albumtype == 'likes':
-    	get_user_likes(username, size)
+        get_user_likes(username, size)
     if albumtype == 'collections':
-    	get_user_collections(username, size)
+        get_user_collections(username, size)
     if albumtype == 'all':
         get_user_uploads(username, size)
         get_user_likes(username, size)
